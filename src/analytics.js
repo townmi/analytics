044e7eddfb3e787b10f7cf8b87960c8f1f1e412e
townmi <towne766@126.com>
2015-07-03 08:55:42 +0800

    add new

---
+++ src/analytics.js
@@ -0,0 +1,18 @@
+/**
+ * @author townmi
+ * @date   2015-07-03
+ * @description [data analytics]
+ */
+
+/**
+ * [description]
+ * @return {[type]} [description]
+ */
+(function () {
+
+
+	var baseSrc = "10.100.142.95:3000/";
+
+	var ;
+
+})();

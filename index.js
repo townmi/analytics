754679ef49029c2f7b7d952d6a9320efe29c8a9f
townmi <towne766@126.com>
2016-06-28 00:13:33 +0800

    at home add denpends

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const koa =  require("koa");
+const koa = require("koa");
 const request = require("request");
-request("http://www.sse.com.cn/js/common/ssesuggestdataAll.js", function (error, response, body) {
+
+request("http://www.sse.com.cn/js/common/ssesuggestdataAll.js", function(error, response, body) {
 	if (!error && response.statusCode == 200) {
 
 		var stockNum = null;
 
-		try	{
-			+function () {
+		try {
+			+ function() {
 				eval(body);
 				stockNum = get_alldata();
-			} ();
+			}();
 		} catch (error) {
 
 		}

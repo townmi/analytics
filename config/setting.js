122f3961a2ea5da92b3f0f1e1f6225b644ec6c1c
Harry Tang <harrytang@vipabc.com>
2016-06-28 19:50:51 +0800

    添加配置文件

---
+++ config/setting.js
@@ -0,0 +1,12 @@
+/*!
+ * @author harrytang@vipabc.com 
+ * @date 16/6/28.
+ */
+module.exports = {
+    "host" : "127.0.0.1",
+    "port" : 3306,
+    "user": "root",
+    // vps
+    "password" : "",
+    "db": "stock"
+};
